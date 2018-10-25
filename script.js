ad61bf638d6afb3f1712f6e44b978fbd151b598e
mourad <mourad.inan@hotmail.fr>
2018-10-25 23:49:52 +0200

    Ajout du remove de l'ancienne recherche

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-var query;
-function quest(){
-    var body = document.body;
-    var h1 = body.getElementsByTagName("h1");
-    query = document.getElementById("query").value;
-    call();
+/* script.js > le script qui permet de creer les éléments de la page html qui vont permettre d'afficher correctement les données récupérées */
 
-}
-
-
+/*  Fonction call est appelé lors de la recherche des actualités
+    par rapport à un terme recherché dans la barre de recherche
+    Elle appelle l'api pour récupérer les données et les renvoie vers createStructure.
+*/
 function call(){
-
+    //Supresssion de l'ancienne recherche si elle existe
+    removeLastSearch();
+    var query = document.getElementById("query").value;
     var url = 'https://newsapi.org/v2/everything?' +
               'q='+ query + '&' +
               'language=fr&' +
@@ -18,46 +16,47 @@ function call(){
     .then(function(response) {
         response.json().then(function(data){
             result = data;
-            for(i=0;i<result.articles.length; i++){
-
-                var card = document.createElement("div");
-                card.setAttribute("class", "card col s4");
-
-                var cardImg = document.createElement("div");
-                cardImg.setAttribute("class", "card-image");
-                var img = document.createElement("img");
-                img.setAttribute("src", result.articles[i].urlToImage);
-
-                var cardContent = document.createElement("div");
-                cardContent.setAttribute("class", "card-content");
-                var p = document.createElement("p");
-                p.innerHTML = result.articles[i].title;
-
-                var cardAction = document.createElement("div");
-                cardAction.setAttribute("class", "card-action");
-                var a = document.createElement("a");
-                a.setAttribute("href", result.articles[i].urlToImage);
-                a.innerHTML = result.articles[i].title;
-
-                document.getElementById("article").appendChild(card);
-                cardImg.appendChild(img);
-                card.appendChild(cardImg);
-                card.appendChild(cardContent);
-                card.appendChild(cardAction);
-                cardContent.appendChild(p);
-                cardAction.appendChild(a);
-            }
-        })
+            createStructure(result);
+        });
     });
 }
-function myFunction(content, text, id) {
-    var h = document.createElement(content);
-    var t = document.createTextNode(text.title);
-    document.getElementById("articles").appendChild(h);
-    h.setAttribute("class", "col s4 article");
-    h.setAttribute("id", "li" + id);
-    var img = document.createElement("img");
+/* Fonction createStructure s'occupe de créer à partir des données reçues de call() des éléments de la page html
+    paramètre : result : resultat de l'api News */
+function createStructure(result){
+    // Boucle sur les articles de la requête
+    for(i=0;i<result.articles.length; i++){
+        // création d'une div card , div card-image, div card-content, div-action (voir materialize)
+        var card = document.createElement("div");
+        card.setAttribute("class", "card col s4");
 
-    document.getElementsByClassName("card-image").appendChild(img);
-    h.appendChild(t);
+        var cardImg = document.createElement("div");
+        cardImg.setAttribute("class", "card-image");
+        var img = document.createElement("img");
+        img.setAttribute("src", result.articles[i].urlToImage);
+
+        var cardContent = document.createElement("div");
+        cardContent.setAttribute("class", "card-content");
+        var p = document.createElement("p");
+        p.innerHTML = result.articles[i].title;
+
+        var cardAction = document.createElement("div");
+        cardAction.setAttribute("class", "card-action");
+        var a = document.createElement("a");
+        a.setAttribute("href", result.articles[i].urlToImage);
+        a.innerHTML = result.articles[i].title;
+
+        // Construction de l'hiérarchie de l'arbre des éléments html
+        document.getElementById("article").appendChild(card);
+        cardImg.appendChild(img);
+        card.appendChild(cardImg);
+        card.appendChild(cardContent);
+        card.appendChild(cardAction);
+        cardContent.appendChild(p);
+        cardAction.appendChild(a);
+    }
+}
+
+function removeLastSearch(){
+    var article = document.getElementById("article");
+    article.innerHTML = '';
 }
