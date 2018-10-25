1a5a04724a792ea79c090cb79f3606966c3ef009
TALEB34 <taleb.seifeddine@gmail.com>
2018-10-25 16:57:14 +0200

    images et date ajoutés dans les titres de articles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1d196f582fa84a40943803b4f6843690';
+var url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=1d196f582fa84a40943803b4f6843690';
 
 var req = new Request(url);
 var result;
@@ -11,7 +11,7 @@ fetch(url)
 
   function addElement (x, lien) { 
   // creé la div h2 et a
-        var newDiv = document.createElement("h2"); 
+        var newDiv = document.createElement("h5"); 
         var newDiv1 = document.createElement("a"); 
 
          newDiv.appendChild(newDiv1);
@@ -30,9 +30,11 @@ fetch(url)
 
 }
 
-  function addAuthor(x){
+  function addAuthor(x, url){
 
-        var newDiv = document.createElement("h4"); 
+        var newDiv = document.createElement("p");
+        var newDiv1 = document.createElement("img"); 
+        newDiv.appendChild(newDiv1); 
 
   // and give it some content 
         var newContent = document.createTextNode(x); 
@@ -42,23 +44,26 @@ fetch(url)
 
 
   // add the newly created element and its content into the DOM 
-        var currentDiv = document.getElementById("h4"); 
+        var currentDiv = document.getElementById("p"); 
         document.body.insertBefore(newDiv, currentDiv); 
-
+         newDiv1.setAttribute("src", url);
+         newDiv1.setAttribute("width","300px");
+         newDiv1.setAttribute("height","300px");
+         
 
   }
 
   for (var i = 0; i < result.articles.length; i++) {
-      if(result.articles[i].author!=null ){
-
-              addAuthor(result.articles[i].author);
-
-          } else { addAuthor(result.articles[i].name);}
-
-
-              console.log(result.articles[i].url);
 
               addElement(result.articles[i].title, result.articles[i].url);
+
+              if(result.articles[i].publishedAt!=null ){
+
+              addAuthor(result.articles[i].publishedAt, result.articles[i].urlToImage);
+
+          } else { addAuthor(result.articles[i].Author, result.articles[i].urlToImage);}
+              
+
 
       }
      
