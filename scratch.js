39e14ec9c71026d688076523f69f22370cbe8419
anlimou abdou <halimoudu73@hotmail.fr>
2018-10-25 16:55:21 +0200

    introduction de la geolocalisation

--- scratch.js
+++ scratch.js
@@ -2,6 +2,11 @@ var key="ee43a99b93e33ab5579123dc84a94031";
 var result;
 var a='Paris';
 var url='http://api.openweathermap.org/data/2.5/weather?q='+a+'&appid='+key;
+if ("geolocation" in navigator) {
+  /* géolocalisation possible */
+} else {
+  alert("Le service de géolocalisation n'est pas disponible sur votre ordinateur.");
+}
 fetch(url)
     .then( function(response){ response.json()
         .then(function(data){
