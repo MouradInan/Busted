d07c14ebac0803f70b0d29777ff10473deef4e75
anlimou abdou <halimoudu73@hotmail.fr>
2018-10-25 16:51:24 +0200

    second test d'affichage de la météo en fonction de la ville

--- scratch.js
+++ scratch.js
@@ -1,6 +1,10 @@
 var key="ee43a99b93e33ab5579123dc84a94031";
 var result;
-var a='Geneva';
-fetch('http://api.openweathermap.org/data/2.5/weather?q='+a+'&appid='+key)
+var a='Paris';
+var url='http://api.openweathermap.org/data/2.5/weather?q='+a+'&appid='+key;
+fetch(url)
     .then( function(response){ response.json()
-        .then(function(data){result = data;console.log(result)}) } );
+        .then(function(data){
+            result = data;
+            console.log(result);
+            document.getElementById("lol").innerHTML ="In "+result.name+", the weather is "+result.weather[0].main;}) } );
