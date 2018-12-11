7beb14a13e2f632aa26ff186b2f9eea03b275798
mourad <mourad.inan@hotmail.fr>
2018-12-11 03:51:47 +0300

    Fusion des deux apis Méteo et News + géolocalisation météo

--- scratch.js
+++ scratch.js
@@ -1,40 +1,65 @@
 var key="ee43a99b93e33ab5579123dc84a94031";
 var result;
-var a='Paris';
-
-var url='http://api.openweathermap.org/data/2.5/weather?q='+a+'&appid='+key;
-fetch(url)
-    .then( function(response){ response.json()
-        .then(function(data){
-            result = data;
-            console.log(result);
-            var imgmeteo= new Image(100,100) ;
-            switch (result.weather[0].main) {
-                case 'Mist':
-                    imgmeteo.scr='https://mvistatic.com/design/images/meteo/pictos/2017/nuit_P6_a.png';
-                    document.body.appendChild(imgmeteo);
-                    break;
-                  case 'Clouds':
-                      imgmeteo.src='http://www.icône.com/images/icones/1/4/weather-overcast-2.png';
-                      document.body.appendChild(imgmeteo);
-                  case 'Sun':
-                    imgmeteo.scr='http://archives.varmatin.com/media_varmatin/imagecache/article-taille-normale/image/ouch/2013/11/17/6819ceccc4f2587143ed626c2bff46a1.png';
-                    document.body.appendChild(imgmeteo);
-                    break;
-                  case 'Rain':
-                      imgmeteo.src='http://www.icône.com/images/icones/1/4/weather-overcast-2.png';
-                      document.body.appendChild(imgmeteo);
-                      break;
-                case 'Clear':
-                       imgmeteo.src='http://archives.varmatin.com/media_varmatin/imagecache/article-taille-normale/image/ouch/2013/11/17/6819ceccc4f2587143ed626c2bff46a1.png';
-                       document.body.appendChild(imgmeteo);
+var a='Montpellier';
+var url;
+			function getLocation() {
+			  if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+              }
+			}
+			
+			function showPosition(position) {
+                console.log(position.coords.latitude);
+                url='http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+
+                position.coords.longitude+'&apikey='+key;
+                getMeteo();
+            }
+            function showError(error){
+                url='http://api.openweathermap.org/data/2.5/weather?q='+a+'&apikey='+key;
+                getMeteo();
+            }
+function getMeteo(){
+    fetch(url)
+        .then( function(response){ response.json()
+            .then(function(data){
+                result = data;
+                console.log(result);
+                var imgmeteo= new Image(100,100) ;
+                var resultMeteo;
+                switch (result.weather[0].main) {
+                    case 'Mist':
+                        imgmeteo.src='https://mvistatic.com/design/images/meteo/pictos/2017/nuit_P6_a.png';
+                        document.getElementById("meteo").append(imgmeteo);
+                        resultMeteo = 'Brouillard';
+                        break;
+                    case 'Clouds':
+                        imgmeteo.src='http://archives.nicematin.com/media_nicematin/imagecache/article-taille-normale-nm/image/ouch/2013/11/17/b8af1a5900556d2e5f4a643b11199ff7.png';
+                        document.getElementById("meteo").appendChild(imgmeteo);
+                        resultMeteo = 'Nuageux';
+                        break;
+                    case 'Sun':
+                        imgmeteo.scr='http://archives.varmatin.com/media_varmatin/imagecache/article-taille-normale/image/ouch/2013/11/17/6819ceccc4f2587143ed626c2bff46a1.png';
+                        document.getElementById("meteo").appendChild(imgmeteo);
+                        resultMeteo = 'Clair';
+                        break;
+                    case 'Rain':
+                        imgmeteo.src='http://www.icône.com/images/icones/1/4/weather-overcast-2.png';
+                        document.getElementById("meteo").appendChild(imgmeteo);
+                        resultMeteo = 'Pluvieux';
+                        break;
+                    case 'Clear':
+                        imgmeteo.src='http://archives.varmatin.com/media_varmatin/imagecache/article-taille-normale/image/ouch/2013/11/17/6819ceccc4f2587143ed626c2bff46a1.png';
+                        document.getElementById("meteo").appendChild(imgmeteo);
+                        resultMeteo = 'Clair';
                         break;
                     case 'Drizzle':
-                       imgmeteo.src='https://png.pngtree.com/png/18/09/10/pngtree-drizzle-weather-png-clipart_1067586.jpg';
-                       document.body.appendChild(imgmeteo);
+                        imgmeteo.src='https://png.pngtree.com/png/18/09/10/pngtree-drizzle-weather-png-clipart_1067586.jpg';
+                        document.getElementById("meteo").appendChild(imgmeteo);
+                        resultMeteo = 'Couvert';    
                         break;
-                  default:
-                    console.log(imgmeteo);
-                    }
-                    document.body.innerHTML +="<div onload='parseCoords()'> In "+result.name+", the weather is "+result.weather[0].main+"</div>";
-        })});
+                    default:
+                        console.log(imgmeteo);
+                        }
+                        document.getElementById("meteo").innerHTML +="<div onload='parseCoords()'>"+ result.name+ ", "+resultMeteo+"</div>";
+            })});
+}
